0949fd14c06cd907eb39c43e7129acb156c35047
Rodrigo <rodrigo_santos01@hotmail.com.br>
2024-02-13 17:25:29 -0300

    terminei ex15

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,7 +3,7 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtan')
     var res = document.querySelector('div#res')
-    res.innerHTML = (` ${ano - Number(fano.value)}`)
+    var imgid = document.querySelector('div#divimgid')
     if (fano.value.length == 0 || Number(fano.value) > ano){
         alert('[ERRO] Verifique os dados e tente novamente')
     } else {
@@ -18,22 +18,41 @@ function verificar() {
                 img.setAttribute('src', 'bb_M.png')
                 genero = 'Criança'
         } else if (idade < 21){
-            img.setAttribute('src', 'jovem_M.png')
-            genero = 'jovem'
+                img.setAttribute('src', 'jovem_M.png')
+                genero = 'jovem'
         } else if (idade < 50){
-            img.setAttribute('src', 'adulto_M.png')
-            genero = 'adulto'
-        } else if (idade < 110){
+                img.setAttribute('src', 'adulto_M.png')
+                genero = 'adulto'
+        } else if (idade < 111){
                 img.setAttribute('src', 'idoso_M.png')
                 genero = 'idoso'
-        } else{
-            genero = 'Caveira'
-            img.setAttribute('scr', 'caveira.png')
+        } else {
+            img.setAttribute('src', 'caveira.png')
+            genero = 'caveira'
         }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade < 10){ 
+                img.setAttribute('src', 'bb_F.png')
+                genero = 'Criança'
+        } else if (idade < 21){
+                img.setAttribute('src', 'jovem_F.png')
+                genero = 'jovem'
+        } else if (idade < 50){
+                img.setAttribute('src', 'adulto_F.png')
+                genero = 'adulto'
+        } else if (idade < 111){
+                img.setAttribute('src', 'idoso_F.png')
+                genero = 'idoso'
+        } else {
+            img.setAttribute('src', 'caveira.png')
+            genero = 'caveira'
         }
-        res.innerHTML = `${genero} com ${idade} Anos`
-        res.appendChild(img)
+        }
+
+        res.innerHTML = `Detectamos um ${genero} com ${idade} Anos`
+        imgid.appendChild(img)
+    
+
     }
 }
