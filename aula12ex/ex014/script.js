e1891b1cc76fa641408862cbe5e225992e9a4b4c
Rodrigo <rodrigo_santos01@hotmail.com.br>
2024-02-12 21:36:47 -0300

    att

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var msgd = document.getElementById('msgd')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora}h`
+    if (hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+        msgd.innerHTML = 'Bom dia'
+        document.body.style.background = 'hwb(189 7% 4%)'
+    } else if (hora >=12 && hora < 18){
+        img.src = 'tarde.png'
+        msgd.innerHTML = 'Boa tarde'
+    }else {
+        //Boa noite
+        img.src = 'noite.png'
+        msgd.innerHTML = '_Boa noite'
+        document.body.style.background = 'blak'
+    }
+}
