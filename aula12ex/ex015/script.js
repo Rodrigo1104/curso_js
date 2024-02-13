68628ff8bed8f71951426bb00262cbe2e85b4c12
Rodrigo <rodrigo_santos01@hotmail.com.br>
2024-02-13 16:25:26 -0300

    att

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,11 +10,30 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
-            genero = 'Homen'
+            genero = ''
+            if (idade < 10){ 
+                img.setAttribute('src', 'bb_M.png')
+                genero = 'Criança'
+        } else if (idade < 21){
+            img.setAttribute('src', 'jovem_M.png')
+            genero = 'jovem'
+        } else if (idade < 50){
+            img.setAttribute('src', 'adulto_M.png')
+            genero = 'adulto'
+        } else if (idade < 110){
+                img.setAttribute('src', 'idoso_M.png')
+                genero = 'idoso'
+        } else{
+            genero = 'Caveira'
+            img.setAttribute('scr', 'caveira.png')
+        }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
         }
         res.innerHTML = `${genero} com ${idade} Anos`
+        res.appendChild(img)
     }
 }
