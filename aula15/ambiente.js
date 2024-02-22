2a252824c7ad1a5bbb3c27159447e8ab02abfb6f
Rodrigo <rodrigo_santos01@hotmail.com.br>
2024-02-21 21:53:23 -0300

    add aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5 ,8 ,'carlo']
+console.log(num)
+num [3] = 6 //add na posição especifica
+console.log(num)
+num.push = 1 //add no final da lista
+console.log(num)
+num.sort()
+console.log(num)
+vet = num.value
+console.log(vet)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,9 @@
+valores = [8,1,7,4,2,9]
+for(pos=0;pos<valores.length;pos++){
+    console.log(valores[pos])
+}
+valores.sort()
+
+for(pos in valores){
+    console.log(valores[pos])
+}
